e6cc682a7fa3a0dec10a03e071d1b1de43f97078
Caio Jordan <caioj2906@gmail.com>
2022-10-08 20:54:13 +0200

    resnet step

--- model_res_deeper.py
+++ model_res_deeper.py
@@ -158,8 +158,8 @@ class HackathonModel(LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
         #lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
-        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.5)
+        #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.75)
 
         opt = {
             'optimizer': optimizer,
