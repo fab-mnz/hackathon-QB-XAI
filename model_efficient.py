a29b0073c40a477b8c710f1cc461f6f1d1ff19c6
fab-mnz <76779449+fab-mnz@users.noreply.github.com>
2022-10-08 23:49:45 +0200

    EfficientNet

--- model_efficient.py
+++ model_efficient.py
@@ -146,8 +146,10 @@ class HackathonModel(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=5e-4)
-        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
+        #lr_scheduler = torch.optim.lr_scheduler.CossineAnnealingLR(optimizer, T_max=10)
+        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.5)
 
         opt = {
             'optimizer': optimizer,
