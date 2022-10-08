114c3b3743de5919ddd6ef6b3dc7fb622c5a562c
Caio Jordan <caioj2906@gmail.com>
2022-10-08 19:06:42 +0200

    res lr 8.5e-4

--- model_res.py
+++ model_res.py
@@ -213,7 +213,7 @@ class HackathonModel(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=6e-4)
+        optimizer = torch.optim.Adam(self.parameters(), lr=8.5e-4)
         #lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=64)
 
