849ce2508ffaa12e79f53481cd3638dca0e0788f
Caio Jordan <caioj2906@gmail.com>
2022-10-08 21:13:20 +0200

    resnet exp lr 8e-4 gamma 98

--- model_res_deeper.py
+++ model_res_deeper.py
@@ -156,8 +156,8 @@ class HackathonModel(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=2e-3)
-        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.8e-3)
+        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
         #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
         #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.75)
 
