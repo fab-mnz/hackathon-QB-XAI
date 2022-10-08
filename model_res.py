aee639e8a77045f506af13fe74f0fda0dd48bfcc
Caio Jordan <caioj2906@gmail.com>
2022-10-08 19:01:09 +0200

    res cosine

--- model_res.py
+++ model_res.py
@@ -214,7 +214,8 @@ class HackathonModel(LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
+        #lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
 
         opt = {
             'optimizer': optimizer,
