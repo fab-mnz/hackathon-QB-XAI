4d043d85197decbd7f7effc4609f1fa1d6076664
Caio Jordan <caioj2906@gmail.com>
2022-10-08 21:54:53 +0200

    resnet loucura

--- model_res_deeper.py
+++ model_res_deeper.py
@@ -159,10 +159,10 @@ class HackathonModel(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-1)
         #lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
-        #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.5)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.5)
 
         opt = {
             'optimizer': optimizer,
